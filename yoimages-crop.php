53a31af7cfe4723fb8f0e180b87f5d68b3b43487
matteo.cajani <matteo.cajani@gmail.com>
2014-12-27 18:56:51 +0100

    splitting into independent plugins

--- yoimages-crop.php
+++ yoimages-crop.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * Plugin Name: YoImages
+ * Plugin Name: YoImages Crop
  * Plugin URI: http://sirulli.org/yoimages/
- * Description: Better image handling capabilities for Wordpress: image cropping tools and image SEO with automated metadata filling.
+ * Description: Better image handling capabilities for Wordpress: image cropping tools
  * Version: 0.0.1
  * Author: Sirulli
  * Author URI: http://sirulli.org/
@@ -31,7 +31,6 @@ if (! defined ( 'ABSPATH' )) {
 	die ( 'No script kiddies please!' );
 }
 
-define ( 'YOIMG_PATH', dirname ( __FILE__ ) );
-require_once (YOIMG_PATH . '/inc/yoimages-commons/init.php');
-require_once (YOIMG_PATH . '/inc/yoimages-crop/init.php');
-require_once (YOIMG_PATH . '/inc/yoimages-seo/init.php');
+define ( 'YOIMG_CROP_PLUGIN_PATH', dirname ( __FILE__ ) );
+require_once (YOIMG_CROP_PLUGIN_PATH . '/vendor/sirulli/yoimages-commons/inc/init.php');
+require_once (YOIMG_CROP_PLUGIN_PATH . '/inc/init.php');
