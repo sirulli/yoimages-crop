4b65b5b4d04754cd3ab1c2c581e7c3677fe744b5
matteo.cajani <matteo.cajani@gmail.com>
2015-01-18 21:36:18 +0100

    0.0.2

--- yoimages-crop.php
+++ yoimages-crop.php
@@ -4,7 +4,7 @@
  * Plugin Name: YoImages Crop
  * Plugin URI: http://sirulli.org/yoimages/
  * Description: Better image handling capabilities for Wordpress: image cropping tools
- * Version: 0.0.1
+ * Version: 0.0.2
  * Author: Sirulli
  * Author URI: http://sirulli.org/
  * License: GPL2
