1db2de370cf05718bedc72fd79f1ebeb69a075bd
matteo.cajani <matteo.cajani@gmail.com>
2015-02-09 23:52:07 +0100

    setup of fr_FR translations

--- yoimages-crop.php
+++ yoimages-crop.php
@@ -4,7 +4,7 @@
  * Plugin Name: YoImages Crop
  * Plugin URI: http://sirulli.org/yoimages/
  * Description: Better image handling capabilities for Wordpress: image cropping tools
- * Version: 0.0.4
+ * Version: 0.0.5
  * Author: Sirulli
  * Author URI: http://sirulli.org/
  * License: GPL2
