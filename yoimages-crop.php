3719013880949b68841b254f161ff8c96524df64
matteo.cajani <matteo.cajani@gmail.com>
2015-06-03 19:38:17 +0200

    ready to merge search branch into master branch

--- yoimages-crop.php
+++ yoimages-crop.php
@@ -4,7 +4,7 @@
  * Plugin Name: YoImages Crop
  * Plugin URI: http://sirulli.org/yoimages/
  * Description: Better image handling capabilities for Wordpress: image cropping tools
- * Version: 0.0.7
+ * Version: 0.1.0
  * Author: Sirulli
  * Author URI: http://sirulli.org/
  * License: GPL2
