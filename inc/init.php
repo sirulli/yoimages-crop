9af2039c1b78ab399a649a4794cf45a1266f3076
Matteo Cajani <matteo.cajani@gmail.com>
2016-09-08 23:24:19 +0200

    fixing requires when module is not active

--- inc/init.php
+++ inc/init.php
@@ -19,8 +19,8 @@ if (is_admin () || php_sapi_name () == 'cli') {
 	define ( 'YOIMG_DEFAULT_CROP_RETINA_ENABLED', FALSE );
 	define ( 'YOIMG_CROP_RETINA_ENABLED', $yoimg_crop_settings && isset ( $yoimg_crop_settings ['retina_cropping_is_active'] ) ? $yoimg_crop_settings ['retina_cropping_is_active'] : YOIMG_DEFAULT_CROP_RETINA_ENABLED );
 	define ( 'YOIMG_CROP_URL', plugins_url ( plugin_basename ( YOIMG_CROP_PATH ) ) );
+	require_once (YOIMG_CROP_PATH . '/utils.php');
 	if (YOIMG_CROP_ENABLED) {
-		require_once (YOIMG_CROP_PATH . '/utils.php');
 		require_once (YOIMG_CROP_PATH . '/image-editor.php');
 		require_once (YOIMG_CROP_PATH . '/extend-admin-media.php');
 		require_once (YOIMG_CROP_PATH . '/extend-admin-media-lightbox.php');
